5b68de932dcda93e269d22f2cfc966f690cd22ce
amataku <amataku13@gmail.com>
2018-03-02 19:17:03 +0900

    do PGPtest

---
+++ PGPtest/pgptest.js
@@ -0,0 +1,44 @@
+let openpgp = require('openpgp')
+
+openpgp.initWorker({path:'openpgp.worker.js'})
+openpgp.config.aead_protect = true
+
+//鍵の作成に必要なoptionの設定
+let options = {
+  userIds:[{name:'example' , email:'hoge@example.com'}],
+  numBit:256,
+}
+
+//鍵の作成
+openpgp.generateKey(options).then(function(key){
+  let privkey = key.privateKeyArmored
+  let pubkey = key.publicKeyArmored
+
+  let privKeyobj = openpgp.key.readArmored(privkey).keys[0]
+
+  //暗号化に必要なoptionの設定
+  let cryptoption = {
+    data: 'Hello, World!',
+    publicKeys: openpgp.key.readArmored(pubkey).keys
+    //privateKeys: privKeyobj
+  }
+
+  //暗号化
+  openpgp.encrypt(cryptoption).then(function(ciphertext){
+    let encrypted = ciphertext.data
+    console.log(encrypted)
+
+    //復号に必要なoptionの設定
+    let decryptoption = {
+      message: openpgp.message.readArmored(encrypted),
+      //publicKeys: openpgp.key.readArmored(pubkey).keys,
+      privateKey: privKeyobj
+    }
+
+    //復号
+    openpgp.decrypt(decryptoption).then(function(plaintext){
+      let plain = plaintext.data
+      console.log(plain)
+    })
+  })
+})
