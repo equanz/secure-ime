bfb260cbcc7145916486880c85bd1342823fb800
amataku <amataku13@gmail.com>
2018-03-09 17:21:10 +0900

    add clip process

---
+++ src/clip.js
@@ -0,0 +1,11 @@
+//使用方法についてはtest/clipboard/clip.html参照
+//clipboard使用の設定
+var clipboard = new ClipboardJS('.btn')
+
+clipboard.on('success',function(e){
+  //成功時の処理
+  e.clearSelection() //テキストの選択解除
+})
+clipboard.on('error',function(){
+  //失敗時の処理
+})
