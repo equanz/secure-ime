f38d09655027485c360fffd43de6fd8760dbdfdc
equanz <equanz324@gmail.com>
2018-03-10 03:39:31 +0900

    feat: add ui, app

--- src/app.js
+++ src/app.js
@@ -1,9 +1,37 @@
+import Vue from 'vue/dist/vue.esm'
 import * as pgp_lib from './pgp.js'
 
-// test
-pgp_lib.SearchKey('example@example.com').then((key) => {
-  console.log(key)
-}).catch((err) => {
-  console.error(err)
+/* let mode_toggle = new Vue({
+ *   el: '#mode-toggle',
+ *
+ * })*/
+
+let mode_toggle = new Vue({
+  el: '#mode-toggle',
+  methods: {
+    click: function (e){
+      // delete active class for all
+      for(let child of e.currentTarget.parentNode.parentNode.children){
+        child.classList.remove('active')
+      }
+      // add active class to current target
+      e.currentTarget.parentNode.classList.add('active')
+
+      // change app instance data(encrypt)
+      if(e.currentTarget.parentNode.id === 'encrypt'){
+        app.encrypt = true
+      }
+      else{
+        app.encrypt = false
+      }
+    }
+  }
+})
+
+let app = new Vue({
+  el: '#app',
+  data: {
+    encrypt: true
+  }
 })
 
